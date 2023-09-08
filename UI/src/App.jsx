ad11497d62ab1ef4102234c08a7b7e90f19cfa38
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-08 16:27:07 +0300

    colors

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout";
+import Colors from "./pages/Colors";
 
 import "./App.css";
-import "./styles/components.css"
+import "./styles/components.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}></Route>
+          <Route path="/" element={<Layout />}>
+            <Route path="/color" element={<Colors />}></Route>
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
