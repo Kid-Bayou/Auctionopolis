3aba5c1a83be3e1e9472a1ec9e5ddec72dcab04a
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-11 11:07:18 +0300

    .

--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -6,16 +6,20 @@ function Header() {
     <>
       <header className="header">
         <Link className="header-title" to="/">
-          <h1 className="header-A">A</h1><h2 className="header-A-rest">uctionopolis</h2>
+          <h1 className="header-A">A</h1>
+          <h2 className="header-A-rest">uctionopolis</h2>
         </Link>
+        <div className="search-bar">
+          <input className="search-input" placeholder="Search" />
+          <img src={search} className="search-img" />
+        </div>
         <nav className="header-nav">
-          <div className="search-bar">
-            <input className="search-input" placeholder="Search"/>
-            <img src={search} className="search-img"/>
-          </div>
-
           <NavLink to="/about" className="header-nav-about">
             About
+          </NavLink>
+
+          <NavLink to="/about" className="header-nav-about">
+            Products
           </NavLink>
 
           <NavLink to="/Signup" className="header-nav-signup">
