a7cfe7d480a8b5ce3f9e28afb53a800c93dee11b
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-15 09:12:56 +0300

    search modal

--- UI/src/pages/SearchModal.jsx
+++ UI/src/pages/SearchModal.jsx
@@ -1,11 +1,12 @@
+import cancel from "../assets/icons/cancel.png"
 
 function SearchModal({closeModal}) {
     return (
         <>
             <div className="search-modal-background">
                 <div className="search-modal-container">
-                    <h1 className="search-modal-input">Search</h1>
-                    <button onClick={() => closeModal(false)}>X</button>
+                    <img src={cancel} onClick={() => closeModal(false)} className="search-modal-button" />
+                    <h1 className="search-modal-header">Search</h1>
                     <input type="text" className="search-modal-input" />
                 </div>
             </div>
