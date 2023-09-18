1a962eb0a9b8210c3e7e0fecf59df06ceed79fd2
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-18 11:06:43 +0300

    responsiveness - added a menu

--- UI/src/components/Header.jsx
+++ UI/src/components/Header.jsx
@@ -1,42 +1,76 @@
 import { Link, NavLink } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import search from "../assets/icons/search.png";
 import SearchModal from "../pages/SearchModal";
+import menu from "../assets/icons/menu.png";
+import closeMenu from "../assets/icons/cancel.png";
 
 function Header() {
   const [openModal, setOpenModal] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 699);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <>
-      <header className="header">
-        <Link className="header-title" to="/">
-          <h1 className="header-A">A</h1>
-          <h2 className="header-A-rest">uctionopolis</h2>
-        </Link>
-        <nav className="header-nav">
-          <img
-            src={search}
-            className="search-img"
-            onClick={() => {
-              setOpenModal(true);
-            }}
-          />
+      <div className={isMobile ? "mobile-header" : "desktop-header"}>
+        <header className="header">
+          <div className="header-content">
+            <Link className="header-title" to="/">
+              <h1 className="header-A">A</h1>
+              <h2 className="header-A-rest">uctionopolis</h2>
+            </Link>
+          </div>
+          <div>
+            {isMobile ? (
+              <div className="menu-icon" onClick={handleMenuClick}>
+                <img src={isMenuOpen ? closeMenu : menu} className="menu-img" />
+              </div>
+            ) : (
+              <nav className="header-nav">
 
-          {openModal && <SearchModal closeModal={setOpenModal} />}
+                <img
+                  src={search}
+                  className="search-img"
+                  onClick={() => {
+                    setOpenModal(true);
+                  }}
+                />
 
-          <NavLink to="/about" className="header-nav-about">
-            About
-          </NavLink>
+                {openModal && <SearchModal closeModal={setOpenModal} />}
 
-          <NavLink to="/about" className="header-nav-about">
-            Products
-          </NavLink>
+                <NavLink to="/about" className="header-nav-item">
+                  About
+                </NavLink>
 
-          <NavLink to="/signup" className="header-nav-signup">
-            Sign Up
-          </NavLink>
-        </nav>
-      </header>
+                <NavLink to="/products" className="header-nav-item">
+                  Products
+                </NavLink>
+
+                <NavLink to="/signup" className="header-nav-signup">
+                  Sign Up
+                </NavLink>
+              </nav>
+            )}
+          </div>
+        </header>
+      </div>
     </>
   );
 }
