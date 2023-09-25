8cc8a15d598b8d2fdf2f105edb327b3c1c4b3188
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-25 23:08:44 +0300

    display profile

--- UI/src/buyer/Profile.jsx
+++ UI/src/buyer/Profile.jsx
@@ -1,10 +1,53 @@
+import { useState, useEffect } from "react";
+import { collection, getDocs, where } from "firebase/firestore";
+import { auth, db } from "../config/firebase";
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-    return (
-        <>
-            <h1>Hello!</h1>
-        </>
-    )
+  const [user, setUser] = useState(null);
+  const [userInfo, setUserInfo] = useState(null); 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        fetchUserData(user.email);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const fetchUserData = async (email) => {
+    try {
+      const userQuerySnapshot = await getDocs(
+        collection(db, "user"),
+        where("email", "==", email)
+      );
+
+      if (!userQuerySnapshot.empty) {
+        const userInfo = userQuerySnapshot.docs[0].data();
+        console.log("User role:", userInfo);
+        setUserInfo(userInfo);  
+      } else {
+        console.error("User not found in database");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <p>Name: {userInfo?.firstName} {userInfo?.lastName}</p>
+      <p>Username: {userInfo?.userName}</p>
+      <p>Email: {user?.email}</p>
+    </>
+  );
 }
 
-export default Profile
+export default Profile;
--- UI/src/seller/Profile.jsx
+++ UI/src/seller/Profile.jsx
@@ -1,41 +1,53 @@
-
 import { useState, useEffect } from "react";
+import { collection, getDocs, where } from "firebase/firestore";
 import { auth, db } from "../config/firebase";
-import { collection, getDocs, addDoc, where } from "firebase/firestore";
-import { onAuthStateChanged } from "firebase/auth";
-
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
-
   const [user, setUser] = useState(null);
+  const [userInfo, setUserInfo] = useState(null); 
+  const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         setUser(user);
+        fetchUserData(user.email);
       } else {
         setUser(null);
       }
     });
 
-    return () => unsubscribe(); 
+    return () => unsubscribe();
   }, []);
 
-  if (user) {
-    const { displayName, email } = user;
-    console.log("Name:", displayName);
-    console.log("Email:", email);
-  }
+  const fetchUserData = async (email) => {
+    try {
+      const userQuerySnapshot = await getDocs(
+        collection(db, "user"),
+        where("email", "==", email)
+      );
+
+      if (!userQuerySnapshot.empty) {
+        const userInfo = userQuerySnapshot.docs[0].data();
+        console.log("User role:", userInfo);
+        setUserInfo(userInfo);  
+      } else {
+        console.error("User not found in database");
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error.message);
+    }
+  };
 
   return (
     <>
       <h1>Profile</h1>
-      <p>Name: {user?.firstName} {user?.lastName}</p>
-      <p>Username: {user?.userName}</p>
+      <p>Name: {userInfo?.firstName} {userInfo?.lastName}</p>
+      <p>Username: {userInfo?.userName}</p>
       <p>Email: {user?.email}</p>
     </>
   );
-  
 }
 
 export default Profile;
