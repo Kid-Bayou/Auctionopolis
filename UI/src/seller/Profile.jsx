a7e03e0dc575b25cc797a359790bd556de85b4d6
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-09-25 22:38:19 +0300

    profile

--- UI/src/seller/Profile.jsx
+++ UI/src/seller/Profile.jsx
@@ -1,10 +1,41 @@
 
+import { useState, useEffect } from "react";
+import { auth, db } from "../config/firebase";
+import { collection, getDocs, addDoc, where } from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
+
+
 function Profile() {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe(); 
+  }, []);
+
+  if (user) {
+    const { displayName, email } = user;
+    console.log("Name:", displayName);
+    console.log("Email:", email);
+  }
+
   return (
     <>
       <h1>Profile</h1>
+      <p>Name: {user?.firstName} {user?.lastName}</p>
+      <p>Username: {user?.userName}</p>
+      <p>Email: {user?.email}</p>
     </>
   );
+  
 }
 
 export default Profile;
